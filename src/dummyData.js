720ff1bc7a384780f82cb9945e5af152139d5c5d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-08-27 08:03:36 +0100

    Add dummyData

---
+++ src/dummyData.js
@@ -0,0 +1,68 @@
+const dummyData = [
+  {
+    id: 1,
+    imageUrl: 'https://images.unsplash.com/photo-1482062364825-616fd23b8fc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timestamp: 'July 17th 2017, 12:42:40 pm',
+    author: 'Easy-E',
+    likes: 15,
+    comments: [
+      {
+        text: 'Great article'
+      },
+      {
+        text: 'I\'ve always wanted to know that!'
+      }
+    ]
+  },
+  {
+    id: 2,
+    imageUrl: 'https://cdn.guidingtech.com/imager/media/assets/2019/05/227327/Troubleshoot-and-Automatically-Join-Team-Chat-in-Overwatch_935adec67b324b146ff212ec4c69054f.png?1559150361',
+    description: 'Unable to join team chat in Overwatch? Here are a few ways to automate joining team chat and troubleshoot your computer if the problem still persists.',
+    timestamp: 'July 17th 2018, 12:42:40 pm',
+    author: 'Mykie Stamas',
+    likes: 27,
+    comments: [
+      {
+        text: 'Great article'
+      },
+      {
+        text: 'I\'ve always wanted to know that!'
+      }
+    ]
+  },
+  {
+    id: 3,
+    imageUrl: 'https://cdn.guidingtech.com/imager/media/assets/192865/multi-mon-cropped_935adec67b324b146ff212ec4c69054f.jpg?1524145099',
+    description: 'Game on multiple monitors or a widescreen display? Here are six programs that you should have installed to improve your experience. Boost your performance, fix bugs and make life easier.',
+    timestamp: 'July 17th 2019, 12:42:40 pm',
+    author: 'Leanne Grimes',
+    likes: 5,
+    comments: [
+      {
+        text: 'Great article'
+      },
+      {
+        text: 'I\'ve always wanted to know that!'
+      }
+    ]
+  },
+  {
+    id: 4,
+    imageUrl: 'https://cdn.guidingtech.com/imager/media/assets/2019/08/235758/Headspace-vs.-10-Percent-Happier_935adec67b324b146ff212ec4c69054f.jpg?1566482409',
+    description: 'With attention span on the decline and stress on the rise, we have started looking for meditation apps. These apps claim to help you be mindful and reduce anxiety and stress using guided sessions. Inspired by the success of leaders like Headspace and Calm, a new player has emerged on the scene. ',
+    timestamp: 'July 17th 2019, 12:42:40 pm',
+    author: 'Jack Stella',
+    likes: 142,
+    comments: [
+      {
+        text: 'Great article'
+      },
+      {
+        text: 'I\'ve always wanted to know that!'
+      }
+    ]
+  }
+];
+
+export default dummyData;
