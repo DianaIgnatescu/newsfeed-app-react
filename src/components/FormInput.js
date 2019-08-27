2f80bcb0c927ce767f334e5259938d340e988982
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-08-27 08:16:09 +0100

    Create FormInput component

---
+++ src/components/FormInput.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => {
+  return (
+    <InputGroup>
+      <input className="form-input" onChange={handleChange} {...otherProps} />
+      {label ? (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
+    </InputGroup>
+  );
+};
+
+const InputGroup = styled.div`
+  position: relative;
+  margin: 45px 0;
+
+  .form-input {
+    background: none;
+    color: grey;
+    font-size: 18px;
+    padding: 10px 10px 10px 5px;
+    display: block;
+    width: 100%;
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid grey;
+    margin: 25px 0;
+
+    &:focus {
+      outline: none;
+    }
+
+    &:focus ~ .form-input-label {
+      top: -14px;
+      font-size: 12px;
+      color: black;
+    }
+  }
+
+  input[type='password'] {
+    letter-spacing: 0.3em;
+  }
+
+  .form-input-label {
+    color: grey;
+    font-size: 16px;
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    left: 5px;
+    top: 10px;
+    transition: 300ms ease all;
+    font-family: 'Poppins', sans-serif;
+
+    &.shrink {
+      top: -14px;
+      font-size: 12px;
+      color: black;;
+    }
+  }
+`;
+
+export default FormInput;
