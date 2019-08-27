b1aadb237d55dc4e2d3c286b61e7415952a9f37d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-08-27 08:09:26 +0100

    Add styles

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+  mainTextColor: '#363a3e',
+  accentColor: '#2690EB',
+  backgroundColor: '#F5F6FA',
+  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  containerColor: '#FFFFFF',
+  hoverGrey: '#fafafa'
+};
+
+export default styles;
